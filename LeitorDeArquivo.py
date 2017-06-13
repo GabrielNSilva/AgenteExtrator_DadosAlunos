3438287e03cc71caaac0c9edd932e3d09c314c4d
Cassio Santos <cassiogvstos@hotmail.com>
2017-06-13 15:59:39 -0300

    Atualizando arquivo testedeleituradearquivo

--- LeitorDeArquivo.py
+++ LeitorDeArquivo.py
@@ -2,7 +2,8 @@ arq = open('C:/Users/Cassio/Desktop/ProjetoExtratorDadosAlunos/AgenteExtrator_Da
 texto = []
 texto.append('\nEstá será alista de alunos\n')
 texto.append('--------------------------\n')
-    texto.append('Começando agora os textes com o PYTHON')
+texto.append('Começando agora os textes com o PYTHON')
+texto.append('Continuando com os teste de leitura/escrita/gravacao com PYTHON')
 
 arq.writelines(texto)
 arq.close()
